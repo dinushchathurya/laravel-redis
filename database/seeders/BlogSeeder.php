9b8309950a82160fccdd83ef457a2fd42a01d237
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-02 21:09:14 +0530

    feat: Create BlogSeeder file

---
+++ database/seeders/BlogSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BlogSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('blogs')->insert([
+            'title' => 'First Blog',
+            'sub_header' => 'Sub Header One',
+            'content' => 'Blog Content One'
+        ]);
+    }
+}
