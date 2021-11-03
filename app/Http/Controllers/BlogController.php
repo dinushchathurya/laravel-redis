4b1f43e996e9a1a2a52f720b938a03c09d1ab6a9
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-03 20:23:07 +0530

    feat: Create mathods to update and delete blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Redis;
 
 class BlogController extends Controller
 {
+    /* get single blog */
     public function index($id)
     {
         $cachedBlog = Redis::get('blog_' . $id);
@@ -21,7 +22,7 @@ class BlogController extends Controller
                 'message' => 'Fetched from redis',
                 'data' => $blog,
             ]);
-            
+
         }else {
 
             $blog = Blog::find($id);
@@ -33,5 +34,40 @@ class BlogController extends Controller
                 'data' => $blog,
             ]);
         }
+    }
+
+    /* update blog */
+    public function update(Request $request, $id) {
+
+        $update = Blog::findOrFail($id)->update($request->all());
+        if($update) {
+
+            // Delete blog_$id from Redis
+            Redis::del('blog_' . $id);
+
+            $blog = Blog::find($id);
+            // Set a new key with the blog id
+
+            Redis::set('blog_' . $id, $blog);
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Blog updated',
+                'data' => $blog,
+            ]);
+        }
+
+    }
+
+    /* delete blog */
+    public function delete($id) {
+
+        Blog::findOrFail($id)->delete();
+        Redis::del('blog_' . $id);
+
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'Blog deleted'
+        ]);
     }
 }
