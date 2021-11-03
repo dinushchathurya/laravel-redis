1ecc393c468bc7ca0284272b9d5f4c6dce8a129b
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-03 20:24:09 +0530

    feat: Add routes endpoints for update and delete blog methods

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
 Route::get('/blogs/{id}', [BlogController::class, 'index']);
+Route::put('/blogs/update/{id}', [BlogController::class, 'update']);
+Route::delete('/blogs/delete/{id}', [BlogController::class, 'delete']);
